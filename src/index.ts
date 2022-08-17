39b5275e599468733cae4d213ce88e0c75c1d6d5
ruansenadev <ruansenadev@gmail.com>
2022-08-17 16:47:40 -0300

    initial effort

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,93 @@
-interface JoystickOptions {
-  directions?: 4 | 8;
-  size?: number;
+import { CongruentDirection, mapCongruentAngles } from "./util";
+
+type JoystickOptions = {
+  /** A number of directions, congruents */
+  directions: number;
+  /** A number of miliseconds, throttling delay */
+  throttling: number;
+  /** A number of pixels, square width of joystick */
+  size: number;
+  /** A boolean for drawing flares indicating current direction */
+  flares: boolean;
+  /** A number of pixels (0 - padding), limit joystick pointer can be drawn */
+  bleeding: number;
+  /** A number of pixels (0 - 16), space between joystick and margin */
+  padding: number;
+  /** A number of pixels (0 - 48), free space to hold the joystick */
+  margin: number;
+  /** A boolean for drawing an assistive sticker, drawn in the margin */
+  ghostSticker: boolean;
+};
+
+type StateObject = {
+  /** A boolean if joystick is being hold */
+  isActive: boolean;
+  /** The current direction index, or null */
+  direction?: string | number;
+  /** A number of current computed angle on the joystick arc */
+  angle?: number;
+  /** A number derived from angle on a single circle turn */
+  absoluteAngle?: number;
+  /** A number of angle cosine */
+  cos?: number;
+  /** A number of angle sine */
+  sin?: number;
+};
+
+type StateChangeCallback = 
+/** @param state - The current state object output */
+(state: StateObject) => void;
+
+const defaultOptions: JoystickOptions = {
+  directions: 8,
+  throttling: 150,
+  flares: true,
+  size: 42,
+  padding: 8,
+  bleeding: 8,
+  margin: 32,
+  ghostSticker: true,
+};
+
+class Joystick {
+  public directions: CongruentDirection[];
+
+  /**
+   * @param selector - A query string or the element itself
+   * @param handler - A callback function called on state change
+   * @param options - An object of joystick options
+   */
+  constructor(selector: string | Element, handler: StateChangeCallback, options: Partial<JoystickOptions> = {}) {
+    let parent = typeof selector == "string" ? document.querySelector(selector) : selector;
+    this.directions = mapCongruentAngles(options.directions ?? defaultOptions.directions);
+  }
+
+  /* private enable() {
+    // set directions angles
+  }
+  private sync() {
+    // handle events and sync state
+  }
+  private animate() {
+    // request animations with cur state
+  } */
 }
 
-class JoystickController {
-  constructor(options: JoystickOptions) {}
+class State {
+  constructor() {}
+  /* track() {
+    // init tracking on touchstart and removes listener
+  }
+  update() {
+    // keeps checking changedTouches of touchmove ev
+  }
+  clear() {
+    // finish tracking when touch list is 1 length on touchend and adds start listener
+  } */
 }
 
-export default JoystickController;
+class Renderer {
+  constructor() {}
+}
+
+export default Joystick;
---
+++ src/util.ts
@@ -0,0 +1,42 @@
+export type CongruentDirection = {
+  /** A number of direction slice start position */
+  startAngle: number;
+  /** A number of direction slice end position */
+  endAngle: number;
+  /** A key index, defaults to counter from 1 */
+  key: string | number;
+};
+
+/**
+ * Maps congruent slices of a circle turn
+ * @param slices - A number, or an indexed array, of congruent slices on a whole circle
+ * @param startAngle - A number of start position on the circle, defaults to top (-0.5 * Math.PI)
+ * @returns A list of direction slices
+ */
+export function mapCongruentAngles(slices: number | string[] | number[], startAngle = -0.5 * Math.PI) {
+  const dirAngles: CongruentDirection[] = [];
+  let slicesTotal = Array.isArray(slices) ? slices.length : slices;
+  let key: number | string;
+  const congruentSliceAngle = (2 * Math.PI) / slicesTotal;
+  for (let i = 0, curAngle = absoluteAngle(startAngle - congruentSliceAngle / 2), endAngle; i < slicesTotal; i++) {
+    key = Array.isArray(slices) ? slices[i] : i + 1;
+    endAngle = absoluteAngle(curAngle + congruentSliceAngle);
+    dirAngles[i] = { startAngle: curAngle, endAngle, key };
+    curAngle = endAngle;
+  }
+  return dirAngles;
+}
+
+/**
+ * Gets the absolute refer angle
+ * @param angle - The position on a circle
+ * @returns An absolute angle between 0 and 2 * Math.PI
+ */
+export function absoluteAngle(angle: number) {
+  if (angle < 0) {
+    // diff from the remainder of a whole circle to a whole circle
+    return (Math.abs(angle) % (Math.PI * 2)) * -1 + Math.PI * 2;
+  }
+  // remainder of a whole circle
+  return angle % (Math.PI * 2);
+}
