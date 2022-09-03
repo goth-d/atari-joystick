17be9347d4ab5794e80801a9360e4eaeb8ab0d86
ruansenadev <ruansenadev@gmail.com>
2022-09-02 23:23:26 -0300

    touch tracking class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { CongruentDirection, mapCongruentAngles } from "./util";
+import { absoluteAngle, CongruentDirection, mapCongruentAngles } from "./util";
 
 type JoystickOptions = {
   /** A number of directions, congruents */
   directions: number;
-  /** A number of miliseconds, throttling delay */
+  /** A number of milliseconds, of delay, for computing moves */
   throttling: number;
   /** A string for the position on the horizontal axis */
   xPosition: "left" | "right";
@@ -30,10 +30,8 @@ type StateObject = {
   isActive: boolean;
   /** The current direction index, or null */
   direction?: string | number;
-  /** A number of current computed angle on the joystick arc */
+  /** A number of current computed touch angle */
   angle?: number;
-  /** A number derived from angle on a single circle turn */
-  absoluteAngle?: number;
   /** A number of angle cosine */
   cos?: number;
   /** A number of angle sine */
@@ -60,10 +58,11 @@ const defaultOptions: JoystickOptions = Object.create({
 
 class Joystick {
   public parent: HTMLElement;
+  private stateCb: StateChangeCallback;
   public directions: CongruentDirection[];
-  private shouldMove = true;
   public isEnabled = true;
-  private touchMoveHandler: (event: TouchEvent) => void;
+  private touchHandler: JoystickTouchHandler;
+  private _throttling?: number;
 
   /**
    * @param element - A query string or the element itself
@@ -72,12 +71,14 @@ class Joystick {
    */
   constructor(element: string | HTMLElement, handler: StateChangeCallback, options: Partial<JoystickOptions> = {}) {
     let parent = typeof element == "string" ? document.querySelector(element) : element;
-    if (!parent || !(parent instanceof HTMLElement)) {
+    if (!(parent instanceof HTMLElement)) {
       throw new ReferenceError(
         `No matches for: ${element},\n Make sure the query is correct and the DOM content has been loaded`
       );
     }
     this.parent = parent;
+
+    this.stateCb = handler;
 
     let optionsObj: Partial<JoystickOptions> & { [k: string]: any } & Object = new Object(options);
     let parsedOptions: { [k: string]: any } & JoystickOptions = Object.create(defaultOptions);
@@ -87,16 +88,8 @@ class Joystick {
 
     this.directions = mapCongruentAngles(parsedOptions.directions ?? 1);
 
-    this.touchMoveHandler =
-      typeof options.throttling == "number"
-        ? (event) => {
-            if (this.shouldMove) {
-              this.shouldMove = false;
-              handler(this.sync(event));
-              setTimeout(() => (this.shouldMove = true), options.throttling);
-            }
-          }
-        : (event) => handler(this.sync(event));
+    this._throttling = options.throttling;
+    this.touchHandler = new JoystickTouchHandler(this);
 
     if (!parsedOptions.startDisabled) {
       this.enable();
@@ -105,23 +98,30 @@ class Joystick {
     }
   }
 
+  /**
+   * Used for calling handler setter to update its move method
+   * @param throttling - A number of milliseconds, of delay, for computing moves, or undefined if no delay
+   */
+  public set throttling(throttling: number | undefined) {
+    this._throttling = throttling;
+    this.touchHandler.move = throttling;
+  }
+  public get throttling() {
+    return this._throttling;
+  }
+
   private enable() {
-    this.parent.addEventListener("touchstart", this.sync);
-    this.parent.addEventListener("touchmove", this.touchMoveHandler);
-    this.parent.addEventListener("touchend", this.sync);
+    this.touchHandler.attachListeners();
     this.isEnabled = true;
   }
-  public sync(event: TouchEvent): StateObject {
-    return {} as StateObject;
-    // handle events and sync state
+  public sync(eventType: TouchEvent["type"], touch: Touch) {
+    this.stateCb({} as StateObject);
   }
   private animate() {
     // request animations with cur state
   }
   private disable() {
-    this.parent.removeEventListener("touchstart", this.sync);
-    this.parent.removeEventListener("touchmove", this.touchMoveHandler);
-    this.parent.removeEventListener("touchend", this.sync);
+    this.touchHandler.dettachListeners();
     this.isEnabled = false;
   }
 }
@@ -144,10 +144,10 @@ class PointPosition {
   public pageY: number;
 
   /**
-   * @param x A number of left position relative to its parent
-   * @param y A number of top position relative to its parent
-   * @param parentX A number of its parent left position relative to page
-   * @param parentY A number of its parent top position relative to page
+   * @param x - A number of left position relative to its parent
+   * @param y - A number of top position relative to its parent
+   * @param parentX - A number of its parent left position relative to page
+   * @param parentY - A number of its parent top position relative to page
    */
   constructor(x: number, y: number, parentX = 0, parentY = 0) {
     this.x = x;
@@ -158,33 +158,128 @@ class PointPosition {
     this.pageY = this.parentY + this.y;
   }
   /**
-   * Calcs a point in the cartesian plane
-   * @param origin A {@link PointPosition} representing 0, 0
-   * @returns An object with x and y coord numbers
+   * Calcs the axes deltas of two points on the cartesian plane
+   * @param pointB - A {@link PointPosition}
+   * @returns An object with x and y, delta, numbers
    */
-  public getRelativeCoords(origin: PointPosition): { x: number; y: number } {
+  public getAxesDeltas(pointB: PointPosition): { x: number; y: number } {
     // Y axis is inverted in page coord
     return Object.create({
-      x: this.pageX - origin.pageX,
-      y: origin.pageY - this.pageY,
+      x: this.pageX - pointB.pageX,
+      y: pointB.pageY - this.pageY,
     });
   }
 
   /**
-   * Calcs an angle of two points, in the cartesian plane, clockwise from right
-   * @param origin A {@link PointPosition} representing 0, 0
+   * Calcs an angle relative to this, in the cartesian plane, clockwise from right
+   * @param point - A {@link PointPosition}
    * @returns A number of the angle
    */
-  public getPointAngle(origin: PointPosition) {
-    let { x: deltaX, y: deltaY } = this.getRelativeCoords(origin);
+  public getPointAngle(point: PointPosition) {
+    let { x: deltaX, y: deltaY } = point.getAxesDeltas(this);
     let thetaAngle = Math.atan2(deltaY, deltaX);
 
-    return thetaAngle;
+    return absoluteAngle(thetaAngle);
   }
 }
 
 class JoystickTouchHandler {
-  constructor() {}
+  public joystick: Joystick;
+  private trackingTouch?: Touch["identifier"];
+  private shouldComputeMove = true;
+  private moveHandler: (event: TouchEvent) => void;
+
+  /**
+   * @param joystick - A {@link Joystick} instance
+   */
+  constructor(joystick: Joystick) {
+    this.joystick = joystick;
+
+    this.moveHandler =
+      typeof this.joystick.throttling == "number"
+        ? (event: TouchEvent) => {
+            if (this.shouldComputeMove) {
+              this.shouldComputeMove = false;
+              this.handleEventsAndSync(event);
+              setTimeout(() => (this.shouldComputeMove = true), this.joystick.throttling);
+            }
+          }
+        : (event: TouchEvent) => this.handleEventsAndSync(event);
+  }
+
+  /** This method should'nt be called directly, instead use {@link Joystick.throttling} */
+  public set move(throttling: number | undefined) {
+    this.dettachListeners();
+
+    this.moveHandler =
+      typeof throttling == "number"
+        ? (event: TouchEvent) => {
+            if (this.shouldComputeMove) {
+              this.shouldComputeMove = false;
+              this.handleEventsAndSync(event);
+              setTimeout(() => (this.shouldComputeMove = true), throttling);
+            }
+          }
+        : (event: TouchEvent) => this.handleEventsAndSync(event);
+
+    this.attachListeners();
+  }
+
+  private handleEventsAndSync(event: TouchEvent) {
+    event.preventDefault();
+    let touch: Touch | null;
+
+    if (event.type == "touchstart") {
+      if (typeof this.trackingTouch == "undefined") {
+        touch = event.changedTouches.item(0);
+
+        if (touch) {
+          this.trackingTouch = touch.identifier;
+          this.joystick.sync(event.type, touch);
+        }
+      }
+    } else if (event.type == "touchmove") {
+      for (let i = 0; i < event.changedTouches.length; i++) {
+        touch = event.changedTouches.item(i);
+
+        if (touch && touch.identifier === this.trackingTouch) {
+          this.joystick.sync(event.type, touch);
+        }
+      }
+    } else if (event.type == "touchend") {
+      for (let i = 0; i < event.changedTouches.length; i++) {
+        touch = event.changedTouches.item(i);
+
+        if (touch && touch.identifier === this.trackingTouch) {
+          if (event.targetTouches.length) {
+            // changes to another current touch that was initiated on the parent el
+            for (i = 0; i < event.targetTouches.length; i++) {
+              touch = event.targetTouches.item(i);
+              if (touch && touch.identifier !== this.trackingTouch) {
+                this.trackingTouch = touch.identifier;
+                this.joystick.sync("touchmove", touch);
+              }
+            }
+          } else {
+            this.trackingTouch = undefined;
+            this.joystick.sync(event.type, touch);
+          }
+        }
+      }
+    }
+  }
+
+  public attachListeners() {
+    this.trackingTouch = undefined;
+    this.joystick.parent.addEventListener("touchstart", this.handleEventsAndSync);
+    this.joystick.parent.addEventListener("touchmove", this.moveHandler);
+    this.joystick.parent.addEventListener("touchend", this.handleEventsAndSync);
+  }
+  public dettachListeners() {
+    this.joystick.parent.removeEventListener("touchstart", this.handleEventsAndSync);
+    this.joystick.parent.removeEventListener("touchmove", this.moveHandler);
+    this.joystick.parent.removeEventListener("touchend", this.handleEventsAndSync);
+  }
 }
 
 export default Joystick;
